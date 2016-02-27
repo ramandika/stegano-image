3ffefb8b07fdfb4a9bee32dbba9b242f64024026
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-02-27 23:55:35 +0700

    Extracting almost done

--- src/main/java/com/company/SteganoAlgorithm.java
+++ src/main/java/com/company/SteganoAlgorithm.java
@@ -178,8 +178,7 @@ public class SteganoAlgorithm {
      */
     public static List<Boolean> convertMessageToBinary(String message) {
         byte[] messageBin = message.getBytes();
-        List<Boolean> messageBitplane = null;
-        //boolean[][] messageMatrix;
+        List<Boolean> messageBitplane = new ArrayList<Boolean>();
         
         for(int x=0; x<messageBin.length; x++) {
             boolean bit;
@@ -225,13 +224,26 @@ public class SteganoAlgorithm {
         return messageBitplane;
     }
     
+    // INI GUE MASIH BINGUNG GIMANA NGECONVERTNYA, TOLONG KERJAIN YA.. :V
+    static boolean[][] convertIntToMatrix(int num) {
+        boolean[][] matrix = new boolean[8][8];
+        byte[] byteNum = new byte[8];
+        for(int i=0; i<8; i++) {
+            byteNum[i] = (byte) ((num >> 7-i) & 0xff);
+            for(int j=0; j<8; j++) {
+                
+            }
+        }
+        return matrix;
+    }
+    
     /**
-     * menghitung jumlah bit yg dapat disimpan
+     * menghitung jumlah byte yg dapat disimpan
      * @param imgPath
      * @param cipher
      * @return 
      */
-    public static int countPayload(String imgPath, String cipher) {
+    public static int countPayloadByte(String imgPath, String cipher) {
         int NbBit = 0;     // jumlah char yang dapat disisipkan pada gambar
         bitplanes = to8x8(new Image(imgPath));
         List<BitPlane> redPBC = toPBC(bitplanes, 'R');
@@ -264,10 +276,39 @@ public class SteganoAlgorithm {
                 }
             }
         }
-        return NbBit;
+        return (NbBit-126)/8;
     }
     
-    public static Image Insert(String imgPath, List<Boolean> binaryMsg,List<Integer> pos) {
+    static int getSeedFromKey(String key) {
+        int seed = 0;
+        for(int i=0; i<key.length(); i++)
+            seed += (int) key.charAt(i);
+        return seed;
+    }
+    
+    static List<Boolean> binaryToListBoolean(byte[] binaryFile) {
+        List<Boolean> messageBitplane = new ArrayList<Boolean>();
+        
+        for(int x=0; x<binaryFile.length; x++) {
+            boolean bit;
+            for(int i=0; i<8; i++) {
+                bit = ((binaryFile[x] & 0xff) & (1 << 7-i))!=0;
+                messageBitplane.add(bit);
+            }
+        }
+        return messageBitplane;
+    }
+    
+    public static Image insertFile(String imgPath, String headerFile, byte[] binaryFile, String key) {
+        int idxSeq = -1;
+        byte[] cipher = CipherTools.encryptFileVigenere(binaryFile, key);
+        List<Boolean> binaryMsg = binaryToListBoolean(cipher);
+        List<boolean[][]> msgMatrix = new ArrayList();
+        msgMatrix.add(convertIntToMatrix(headerFile.length()));                       // parameternya info size header, 0 karena bukan nyisipin file
+        msgMatrix.add(convertIntToMatrix(binaryFile.length));                         // parameternya info size dr text yang akan di-embed
+        msgMatrix.addAll(convertMessageToMatrix(binaryMsg));
+        List<Integer> randSeq = getUniqRandSeq(getSeedFromKey(key), msgMatrix.size());
+        
         bitplanes = to8x8(new Image(imgPath));
         List<BitPlane> redPBC = toPBC(bitplanes, 'R');
         List<BitPlane> greenPBC = toPBC(bitplanes, 'G');
@@ -275,34 +316,148 @@ public class SteganoAlgorithm {
         List<BitPlane> redCGC = XOR(redPBC);
         List<BitPlane> greenCGC = XOR(greenPBC);
         List<BitPlane> blueCGC = XOR(bluePBC);
-//        for(int i=0; i<redCGC.size(); i++) {                        // seluruh blok 8x8
-//            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
-//                for(int n=0; n<redCGC.get(i).bp.size(); n++) {      // 8 layer bit plane
-//                    if(isComplexEnough(redCGC.get(i).bp.get(n)))
-//                        NbChar += 7;
-//                }
-//            }
-//        }
-//        for(int i=0; i<greenCGC.size(); i++) {                        // seluruh blok 8x8
-//            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
-//                for(int n=0; n<greenCGC.get(i).bp.size(); n++) {      // 8 layer bit plane
-//                    if(isComplexEnough(greenCGC.get(i).bp.get(n)))
-//                        NbChar += 7;
-//                }
-//            }
-//        }
-//        for(int i=0; i<blueCGC.size(); i++) {                        // seluruh blok 8x8
-//            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
-//                for(int n=0; n<blueCGC.get(i).bp.size(); n++) {      // 8 layer bit plane
-//                    if(isComplexEnough(blueCGC.get(i).bp.get(n)))
-//                        NbChar += 7;
-//                }
-//            }
-//        }
+        for(int i=0; i<redCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<redCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(redCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                redCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                redCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<greenCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<greenCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(greenCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                greenCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                greenCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<blueCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<blueCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(blueCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                blueCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                blueCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        redPBC = XOR(redCGC);
+        greenPBC = XOR(greenCGC);
+        bluePBC = XOR(blueCGC);
+        
+        // BALIKIN KE GAMBAR
+        // MASIH BINGUNG
+        // TOLONG LANJUTIN YEEE
+        
+        return null;
+    }
+    
+    public static Image insertText(String imgPath, String message, String key) {
+        int idxSeq = -1;
+        String cipher = CipherTools.encryptVigenereExtended(message, key);
+        List<Boolean> binaryMsg = convertMessageToBinary(cipher);
+        List<boolean[][]> msgMatrix = new ArrayList();
+        msgMatrix.add(convertIntToMatrix(0));                       // parameternya info size header, 0 karena bukan nyisipin file
+        msgMatrix.add(convertIntToMatrix(message.length()));        // parameternya info size dr text yang akan di-embed
+        msgMatrix.addAll(convertMessageToMatrix(binaryMsg));
+        List<Integer> randSeq = getUniqRandSeq(getSeedFromKey(key), msgMatrix.size());
+        
+        bitplanes = to8x8(new Image(imgPath));
+        List<BitPlane> redPBC = toPBC(bitplanes, 'R');
+        List<BitPlane> greenPBC = toPBC(bitplanes, 'G');
+        List<BitPlane> bluePBC = toPBC(bitplanes, 'B');
+        List<BitPlane> redCGC = XOR(redPBC);
+        List<BitPlane> greenCGC = XOR(greenPBC);
+        List<BitPlane> blueCGC = XOR(bluePBC);
+        for(int i=0; i<redCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<redCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(redCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                redCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                redCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<greenCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<greenCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(greenCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                greenCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                greenCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0; i<blueCGC.size(); i++) {                        // seluruh blok 8x8
+            if(((i+1)%wh!=0)&&((i/wh)!=he-1)) {
+                for(int j=0; j<blueCGC.get(i).bp.size(); j++) {      // 8 layer bit plane
+                    if(isComplexEnough(blueCGC.get(i).bp.get(j))) {
+                        idxSeq++;
+                        if(randSeq.contains(idxSeq)) {
+                            if(isComplexEnough(msgMatrix.get(randSeq.indexOf(idxSeq)))) {
+                                blueCGC.get(i).bp.set(j, msgMatrix.get(randSeq.indexOf(idxSeq)));
+                            }
+                            else {
+                                blueCGC.get(i).bp.set(j, conjugate(msgMatrix.get(randSeq.indexOf(idxSeq))));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        redPBC = XOR(redCGC);
+        greenPBC = XOR(greenCGC);
+        bluePBC = XOR(blueCGC);
+        
+        // BALIKIN KE GAMBAR
+        // MASIH BINGUNG
+        // TOLONG LANJUTIN YEEE
+        
         return null;
     }
 
-    public List<Integer> genUniqRandSeq(int seed, int size){
+    static List<Integer> getUniqRandSeq(int seed, int size){
         List<Integer> list=new ArrayList();
         for(int i=0;i<size;i++)
             list.add(i);
