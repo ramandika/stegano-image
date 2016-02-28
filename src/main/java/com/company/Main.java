5d09189a08b4c7511b1457583941e24ff9e55850
Rakhmatullah Yoga Sutrisna <rakhmatullahyoga@gmail.com>
2016-02-28 13:53:53 +0700

    gabungin GUI

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -14,34 +14,34 @@ import java.util.List;
 
 public class Main {
     
-    public static void main(String[] args) throws FileNotFoundException, IOException,Exception {
-/*        File file = new File("data/message/foto.jpg");
-        byte[] fileData = new byte[(int)file.length()];
-        FileInputStream in = new FileInputStream(file);
-        in.read(fileData);
-        in.close();
-        byte[] encrypted = CipherTools.encryptFileVigenere(fileData, "yoga");
-        FileOutputStream outputStream = new FileOutputStream(new File("data/message/encrypted.jpg"));
-        outputStream.write(encrypted);
-        outputStream.close();
-        // ========= DECRYPT ==============
-        file = new File("data/message/encrypted.jpg");
-        fileData = new byte[(int)file.length()];
-        in = new FileInputStream(file);
-        in.read(fileData);
-        in.close();
-        encrypted = CipherTools.decryptFileVigenere(fileData, "yoga");
-        outputStream = new FileOutputStream(new File("data/message/decrypted.jpg"));
-        outputStream.write(encrypted);
-        outputStream.close();
-        byte[] strBin = "Hello".getBytes();
-        for(byte b : strBin) {
-            for(int i=0; i<8; i++) {
-                System.out.print(Integer.toBinaryString((b & 0xff) & (1 << 7-i))+' ');
-            }
-            System.out.println();
-        }*/
-        FuncTest.testConjugate();
-    }
+//    public static void main(String[] args) throws FileNotFoundException, IOException,Exception {
+//        File file = new File("data/message/foto.jpg");
+//        byte[] fileData = new byte[(int)file.length()];
+//        FileInputStream in = new FileInputStream(file);
+//        in.read(fileData);
+//        in.close();
+//        byte[] encrypted = CipherTools.encryptFileVigenere(fileData, "yoga");
+//        FileOutputStream outputStream = new FileOutputStream(new File("data/message/encrypted.jpg"));
+//        outputStream.write(encrypted);
+//        outputStream.close();
+//        // ========= DECRYPT ==============
+//        file = new File("data/message/encrypted.jpg");
+//        fileData = new byte[(int)file.length()];
+//        in = new FileInputStream(file);
+//        in.read(fileData);
+//        in.close();
+//        encrypted = CipherTools.decryptFileVigenere(fileData, "yoga");
+//        outputStream = new FileOutputStream(new File("data/message/decrypted.jpg"));
+//        outputStream.write(encrypted);
+//        outputStream.close();
+//        byte[] strBin = "Hello".getBytes();
+//        for(byte b : strBin) {
+//            for(int i=0; i<8; i++) {
+//                System.out.print(Integer.toBinaryString((b & 0xff) & (1 << 7-i))+' ');
+//            }
+//            System.out.println();
+//        }
+//        FuncTest.testConjugate();
+//    }
     
 }
